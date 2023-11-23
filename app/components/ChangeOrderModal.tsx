9f255fa6dc6395663bc3296011bd10e6963d1a37
ashcon minoo <acminoo@gmail.com>
2023-11-22 23:25:00 -0500

    added modal btn, still haven't implemented modal

--- app/components/ChangeOrderModal.tsx
+++ app/components/ChangeOrderModal.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 export default function ChangeOrderModal() {
+
+const [modalOpen, setModalOpen] = useState(false);
+// const [formData, setFormData] = useState<>
+
+const showModal = () => {
+  setModalOpen(!modalOpen);
+}
+
+// const handleSubmit = () => {
+//   onsubmit(formData)
+// }
+
   return (
     <div>
-      
+      <button className={`modal-btn bg-blue-500 text-white font-bold py-2 px-4 ${modalOpen ? 'active' : ''}`}
+      onClick={showModal}>Show Modal</button>
+      {modalOpen && (
+        <div id='modal'>
+          <p>Hello</p>
+        </div>
+      )}
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+"use client"
 import ChangeOrderList from "./components/ChangeOrderList"
+import ChangeOrderModal from "./components/ChangeOrderModal"
 
 export default function Home() {
   return (
     <div id='app'>
+      <ChangeOrderModal/>
       <ChangeOrderList/>
     </div>
   )
