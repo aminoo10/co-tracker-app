85727265a2527b515c22dfb0d0e953e9734e70e8
ashcon minoo <acminoo@gmail.com>
2024-06-29 16:06:07 -0400

    removed the changing MES button cause it didn't really make sense

--- app/components/ChangeOrderList.tsx
+++ app/components/ChangeOrderList.tsx
@@ -99,12 +99,22 @@ export default function ChangeOrderList({COList, onDelete, getCHGObject, onEdit,
           <p>{changeOrder.environment}</p>
 
           <div className={`${(GET_RISK_NAMES(changeOrder.risk))} h-full w-full flex items-center justify-center relative`}>
-            <span className='focus:outline-none px-3 py-1 bg-red-500 text-white absolute -top-4 -right-4'>!</span>
+
+            <span data-tooltip-target='tooltip-default' className='px-3 py-1 bg-red-500 hover:bg-red-700 text-white absolute -top-4 -right-3'>!</span>
+            
+            <div id="tooltip-default" role='tooltip' className='absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700'>
+              Test1234
+              <div className='tooltip-arrow' data-popper-arrow></div>
+            </div>
+
             <p> {changeOrder.risk}</p>
+
           </div>
 
           <p>{changeOrder.description}</p>
-          <button onClick={handleMESProvided}>{(changeOrder.mesProvided) ? "True" : "False"}</button>
+          {/* <button onClick={handleMESProvided}>{(changeOrder.mesProvided) ? "True" : "False"}</button> */}
+          <p>{(changeOrder.mesProvided) ? "True" : "False"}</p>
+
 
           <div id='status' className={`flex justify-between h-full w-full`}>
             <button onClick={handlePrevStatus} className=''>
@@ -130,7 +140,7 @@ export default function ChangeOrderList({COList, onDelete, getCHGObject, onEdit,
               type='button'
               className="focus:outline-none modal-close px-1 py-1 bg-blue-500 mb-0.5 text-white hover:bg-blue-700"
               onClick={openEditModal}
-              ><svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+              ><svg className="w-5 h-5 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                 <path fill-rule="evenodd" d="M14 4.182A4.136 4.136 0 0 1 16.9 3c1.087 0 2.13.425 2.899 1.182A4.01 4.01 0 0 1 21 7.037c0 1.068-.43 2.092-1.194 2.849L18.5 11.214l-5.8-5.71 1.287-1.31.012-.012Zm-2.717 2.763L6.186 12.13l2.175 2.141 5.063-5.218-2.141-2.108Zm-6.25 6.886-1.98 5.849a.992.992 0 0 0 .245 1.026 1.03 1.03 0 0 0 1.043.242L10.282 19l-5.25-5.168Zm6.954 4.01 5.096-5.186-2.218-2.183-5.063 5.218 2.185 2.15Z" clip-rule="evenodd"/>
               </svg>
             </button>
@@ -138,7 +148,7 @@ export default function ChangeOrderList({COList, onDelete, getCHGObject, onEdit,
               type='button'
               className="focus:outline-none modal-close px-1 py-1 bg-red-500 text-white hover:bg-red-700"
               onClick={openDeleteModal}
-              ><svg className="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+              ><svg className="w-5 h-5 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                 <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
               </svg>
             </button>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/flowbite-react/lib/**/*.js'
   ],
   theme: {
     extend: {
@@ -43,7 +44,9 @@ const config: Config = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin")
+  ],
 }
 
 export default config
